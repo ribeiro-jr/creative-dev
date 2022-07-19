542c03ad0122ceb7d6174c41d8b4c3f72f399b63
Vanilson Ribeiro <ribeirovanilson85@gmail.com>
2022-07-19 14:11:40 +0100

    feat: add feedback factory & seed

---
+++ database/factories/FeedBackFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FeedBack>
+ */
+class FeedBackFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'phone' => fake()->randomNumber(9),
+            'email' => fake()->unique()->safeEmail(),
+            'subject' => fake()->sentence(),
+            'body' => fake()->text(500),
+        ];
+    }
+}
--- database/seeders/FeedBackSeeder.php
+++ database/seeders/FeedBackSeeder.php
@@ -2,18 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\FeedBack;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
+class FeedBackSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(FeedBackSeeder::class);
+        DB::table('feed_backs')->truncate();
+        FeedBack::factory(7)->create();
     }
 }
